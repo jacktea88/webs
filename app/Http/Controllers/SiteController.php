2f10c09e889ae4baa71bf77ae9b7aa6033df803b
jacktea88 <jacktea@gmail.com>
2022-08-25 15:05:11 +0800

    將符合條件的資料庫資料送到前台頁面

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
     //
     public function renderBlogPage(){
-        return view('blog');
-    return view('welcome');
+        // return view('blog');
+        $posts = Post::where('status','published')->orderBy('created_at','desc')->get();
+        // return $posts;
+
+        return view('blog',compact('posts'));
 
     }
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +26,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        View::composer(['blog','post'], function ($view) {
+        // View::composer(['post'], function ($view) {
+
+            // dd($view);
+            $categories = Category::where('enabled',true)->orderBy('sort','asc')->get();
+            // dd($categories);
+            $view->with('categories',$categories);
+            // dd($view);
+        });
     }
 }
